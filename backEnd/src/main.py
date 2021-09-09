231269a403a0888a1b4db022d68ac32a533f6a50
gruel <gruelg@outlook.com>
2021-09-09 11:39:25 +0200

    fin matinée 09/09

--- backEnd/src/main.py
+++ backEnd/src/main.py
@@ -7,18 +7,18 @@ import json
 config = {
     'host': '127.0.0.1',
     'port': '3306',
-    'user': 'root',
+    'user': 'api',
     'password':'my-secret-pw',
     'database':'Curiculum'
 }
 db_user = config.get('user')
 db_pwd =config.get('password')
 db_host = config.get('host')
-db_port = config.get('port')
+db_port = config.get('port')  
 db_name = config.get('database')
 
 connexion_str = f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
-engine = create_engine(connexion_str)
+engine = create_engine(connexion_str, pool_recycle=3600)
 connexion = engine.connect()
 #metadata
 metadata = MetaData()
